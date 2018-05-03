f79e4e4a80cc1aa11a26f3be3078433347362974
only <776268047@qq.com>
2018-05-03 23:37:01 +0800

    初始化项目结构

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dev: {
+    host: 'http://127.0.0.1',
+    port: '88',
+    path: 'dist',
+    publicPath: './',
+    serverHost : "http://127.0.0.1:8080"
+  },
+  myHost:{
+    path: 'dist',
+    publicPath: './',
+    serverHost : "http://121.199.11.175:8080/boss"
+  }
+}
